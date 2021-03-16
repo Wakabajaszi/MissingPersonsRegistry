0c9cf25bfa0153e596ab71c9a93b94037602a81e
Wakabajaszi <michal.jedrych@outlook.com>
2021-03-16 16:38:27 +0100

    initial asp .net core mvc project

---
+++ MissingPersonsRegistry/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MissingPersonsRegistry.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
