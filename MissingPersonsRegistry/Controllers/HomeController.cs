bb1a4f2ddcb3ee10cda03cfba49dd6b9fda3be40
Wakabajaszi <michal.jedrych@outlook.com>
2021-03-17 15:16:02 +0100

    Add image add funtion to create action

--- MissingPersonsRegistry/Controllers/HomeController.cs
+++ MissingPersonsRegistry/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MissingPersonsRegistry.Data;
 using MissingPersonsRegistry.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +15,14 @@ namespace MissingPersonsRegistry.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            this.dbContext = dbContext;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -27,7 +34,19 @@ namespace MissingPersonsRegistry.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return Redirect("Home/Create");
+            }
+            string filePath = UploadFile(person);
+            person.ImageSrc = filePath;
+            
 
+            return Redirect("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -37,6 +56,24 @@ namespace MissingPersonsRegistry.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string UploadFile(Person person) 
+        {
+            string fileName = null;
+            string filePath="";
+            if (person.PersonImage != null) 
+            {
+                string uploadDir = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                string extension = Path.GetExtension(person.PersonImage.FileName);
+                fileName = Guid.NewGuid().ToString() + "-" + $"{person.Name}-{person.Surname}{extension}";
+                filePath = Path.Combine(uploadDir, fileName);
+                using(var filestream = new FileStream(filePath, FileMode.Create)) 
+                {
+                    person.PersonImage.CopyTo(filestream);
+                }
+            }
+            return filePath;
         }
     }
 }
